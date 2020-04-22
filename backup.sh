b4a75893c7edc4168985a9d940d22409cab77380
Ruben Vandamme <github-com-00ff86@vandamme.email>
2020-04-22 20:31:36 +0200

    Add keyexport.

--- backup.sh
+++ backup.sh
@@ -6,3 +6,11 @@ cat /storage/ssh/id_ed25519.pub
 [ -e /config/borgmatic/config.yaml ] || autocreate-borg-repo
 BORG_NEW_PASSPHRASE="" borgmatic -c /config/borgmatic init --append-only --encryption keyfile-blake2 --storage-quota 2G -v2
 borgmatic -c /config/borgmatic create
+if [ ! -e /repo/keyexport ]; then
+	mkdir -p /repo/keyexport
+	cd /repo/keyexport
+	export BORG_BASE_DIR=/repo
+	borg key export $(ls -1 /repo/.config/borg/security/*/location | head -n1 | xargs cat) ${BORGBASE_NAME}.key
+	borg key export --paper $(ls -1 /repo/.config/borg/security/*/location | head -n1 | xargs cat) ${BORGBASE_NAME}.paperkey
+	borg key export --qr-html $(ls -1 /repo/.config/borg/security/*/location | head -n1 | xargs cat) ${BORGBASE_NAME}.html
+fi
